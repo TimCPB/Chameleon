be5406f0729d1e6e8fb4d3fb29ef1d6add443acc
Tim <TimCPB@github.com>
2020-09-21 13:22:54 +0100

    green test - Renders the GitHub username

--- chameleon/src/GithubUser.js
+++ chameleon/src/GithubUser.js
@@ -3,12 +3,29 @@ import React, { Component } from 'react'
 class GithubUser extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      gitHubName: ''
+    }
+  }
+
+  componentDidMount() {
+    const options = {
+      "headers": {
+        "Authorization": "token f24de91b4b4b111558052b4ac7a09b05a499c15d",
+        "Accept": "application/vnd.github.v3+json"
+      }
+    }
+    fetch("https://api.github.com/users/TimCPB", options)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({gitHubName: data.name})
+      })
   }
 
   render() {
     return (
       <div className="User">
-        "Hello, world!"
+        {this.state.gitHubName}
       </div>
     )
   }

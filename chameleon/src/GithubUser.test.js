c067886981a0659cd8b3cfbed5340cdc7da93173
Tim <TimCPB@github.com>
2020-09-21 12:43:45 +0100

    green test - Renders the component

--- chameleon/src/GithubUser.test.js
+++ chameleon/src/GithubUser.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen, waitForElement } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
 
 import GithubUser from './GithubUser'
 
